6d60ade4eee2470ad127f3f191565686a319b486
bintemasood <humairamasood19@gmail.com>
2024-03-10 05:36:10 -0400

    assignment3

---
+++ main.dart
@@ -0,0 +1,42 @@
+void main (){
+//Assignment # 3
+//Q.1: Create two integer variables length and breadth and assign values then check if they are square values or rectangle values.
+//i.e: if both values are equal then it's square otherwise rectangle.
+int length = 5 ;
+int breadth = 5;
+if (length==breadth){
+  print ("it's a Square");
+} else {
+  print ("it's a Rectangle");
+}
+
+//Q.2: Take two variables and store age then using if/else condition to determine oldest and youngest among them.
+int ageA = 47;
+int ageB = 36;
+if(ageA>ageB){
+  print("Nasir is oldest");
+  print("Humaira is youngest");
+} else {
+  print("Nasir is youngest");
+  print ("Humaira is oldest");
+}
+
+//Q.3: Write a Dart program to check if a given number is positive, negative, or zero.
+var num = 54; 
+   if(num > 0) { 
+      print("${num} is positive"); 
+   } 
+   else if(num < 0) { 
+      print("${num} is negative"); 
+   } else { 
+      print("${num} is neither positive nor negative"); 
+   } 
+
+//Create LinkedIn Profile at https://www.linkedin.com/
+//https://www.linkedin.com/in/humaira-masood-191810287?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app
+
+
+//Create Git Profile at https://github.com/
+//https://github.com/bintemasood
+
+}
